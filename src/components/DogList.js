34f3ce30b7b92c4321ea5b39f54184c59187db58
Poppy Meng <qim17@pitt.edu>
2022-06-14 11:04:39 -0700

    class practice

---
+++ src/components/DogList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Dog from "./Dogs";
+
+export const DogList=()=>{
+    return (
+        <ul>
+            <h3>Dog number:</h3>
+            <Dog></Dog>
+            <Dog></Dog>
+        </ul>
+    )
+}
---
+++ src/components/Dogs.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dog=()=>{
+    return (
+        <li>
+            <h1>DogSite</h1>
+            <span>Tag: </span>
+            <button>add tag</button>
+        </li>
+    ) 
+}
+export default Dog;
